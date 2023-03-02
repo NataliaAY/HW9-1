2b4aaf5c4801c4ee74ca287e13c9f7f6a75369ce
Natalia <averegina@male.ru>
2023-03-02 20:06:58 +0300

    решена задача 40

---
+++ 40.py
@@ -0,0 +1,11 @@
+'''
+Работать с файлом california_housing_train.csv, который находится в папке
+sample_data. Определить среднюю стоимость дома (median_house_value), где кол-во людей от 0 до 500 (population).
+'''
+
+df = pd.read_csv('california_housing_train.csv')
+
+df[
+    (df['population'] > 0) &
+    (df['population'] < 500)
+].median_house_value.mean()
